9085bfcbccd78ac4a6a6a85d90e3df9720eea351
ashleyvsch <74679518+ashleyvsch@users.noreply.github.com>
2023-10-03 16:48:55 -0700

    test go

--- tests/test_go.py
+++ tests/test_go.py
@@ -2,3 +2,13 @@ from danrerlib import GO
 from danrerlib.settings import *
 from pandas.testing import assert_frame_equal
 from itertools import permutations
+
+def test_get_genes_from_go_id():
+    go_id = 'GO:0005173'
+    org = 'dre'
+    generated_genes = GO.get_genes_in_GO_concept(go_id, org, 'sym')
+    generated_genes = generated_genes.to_frame().sort_values(by='Symbol', ascending=True).reset_index(drop=True)
+
+    true_genes = pd.read_csv('tests/data/out_data/go/id_dre_0007063.txt')
+    true_genes = true_genes.sort_values(by='Symbol', ascending=True).reset_index(drop=True)
+    assert_frame_equal(true_genes, generated_genes) 
